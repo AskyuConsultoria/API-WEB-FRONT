04c47c6f60448ea90dc62c2c260ab5304444aa5a
Nathan Ribeiro de Araujo Oliveira <nathan.oliveira@sptech.school>
2024-03-06 20:44:15 -0300

    Feat(API):Aplicação em node.js base criada

---
+++ app.js
@@ -0,0 +1,24 @@
+process.env.AMBIENTE_PROCESSO = "desenvolvimento";
+// process.env.AMBIENTE_PROCESSO = "producao";
+
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
+var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
+
+var app = express();
+
+var indexRouter = require("./src/routes/index");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+app.use("/", indexRouter);
+
+app.listen(PORTA, function () {
+    console.log(`Servidor do seu site já está rodando na url http://localhost:${PORTA} \n
+    Você está no ambiente de ${process.env.AMBIENTE_PROCESSO}`);
+});
